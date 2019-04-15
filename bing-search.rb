b29b54c7aa560a732cd67cb9033d33e3b78c162b
muhammad <muhammed.ebeid@gmail.com>
2019-04-15 15:21:02 +0200

    template

---
+++ bing-search.rb
@@ -0,0 +1,30 @@
+
+require 'rest-client'
+
+class BingSearch
+  def initialize(url, query)
+    @data = RestClient.get(url, {:params => {:q => query}})
+  end
+
+  def print_data
+    puts "\nHeaders:\n#{@data.headers}"
+    puts "\nCookies:\n#{@data.cookies}"
+    puts "\nBody:\n#{@data.body}"
+  end
+
+  def print_links
+    raw = @data.body.split('b_algo"><h2><a href="').drop(1)
+    links = []
+    raw.each do |e|
+      temp = e.split('" ')
+      links << temp[0]
+    end
+    links.each {|l| puts l}
+  end
+end
+
+url = 'https://www.bing.com/search'
+query = 'spaceship'
+search = BingSearch.new(url, query)
+
+search.print_links
